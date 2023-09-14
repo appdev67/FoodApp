e08290c1db638d68fb4a5f0d453834396e049fe7
appdev16677 <appdev16677@gmail.com>
2023-09-14 17:44:04 +0300

    test

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-
+//try4
 export default function App() {
   return (
     <View style={styles.container}>
