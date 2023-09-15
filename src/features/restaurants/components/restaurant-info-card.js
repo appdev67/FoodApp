1c1f32e939e68297a021d69ef2dfd72b51c9eebd
appdev16677 <appdev16677@gmail.com>
2023-09-15 21:42:32 +0300

    aaaaa

---
+++ src/features/restaurants/components/restaurant-info-card.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+import styled from "styled-components";
+
+const RestaurantCard = styled(Card)`
+  backgroundcolor: "white";
+`;
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  backgroundcolor: "white";
+`;
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <RestaurantCard>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
