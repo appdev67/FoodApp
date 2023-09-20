c54bbf6425d12b8a287f57a004683f22c93f7abb
appdev16677 <appdev16677@gmail.com>
2023-09-20 10:43:03 +0300

    Loading

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { Text } from "react-native";
-
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
-
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import { SafeArea } from "./src/components/utility/safe-area";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
@@ -14,16 +12,14 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import Ionicons from "react-native-vector-icons/Ionicons";
-
-import { resturantsRequest } from "./src/services/restaurant/restaurants.service"
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "./src/components/utility/safe-area";
+import { RestaurantsContextProvider } from "./src/services/restaurant/restaurants.context";
 
 const Tab = createBottomTabNavigator();
 
-const Tab_Icon = {
+const TAB_ICON = {
   Restaurants: "md-restaurant",
   Map: "md-map",
   Settings: "md-settings",
@@ -31,10 +27,9 @@ const Tab_Icon = {
 
 const Settings = () => (
   <SafeArea>
-    <Text>settings</Text>
+    <Text>Settings</Text>
   </SafeArea>
 );
-
 const Map = () => (
   <SafeArea>
     <Text>Map</Text>
@@ -42,7 +37,7 @@ const Map = () => (
 );
 
 const createScreenOptions = ({ route }) => {
-  const iconName = Tab_Icon[route.name];
+  const iconName = TAB_ICON[route.name];
   return {
     tabBarIcon: ({ size, color }) => (
       <Ionicons name={iconName} size={size} color={color} />
@@ -51,10 +46,11 @@ const createScreenOptions = ({ route }) => {
 };
 
 export default function App() {
-  let [oswaldLoaded] = useOswald({
+  const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
-  let [latoLoaded] = useLato({
+
+  const [latoLoaded] = useLato({
     Lato_400Regular,
   });
 
@@ -65,19 +61,21 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={createScreenOptions}
-            tabBarOptions={{
-              activeTintColor: "tomato",
-              inactiveTintColor: "gray",
-            }}
-          >
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-            <Tab.Screen name="Map" component={Map} />
-            <Tab.Screen name="Settings" component={Settings} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator
+              screenOptions={createScreenOptions}
+              tabBarOptions={{
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray",
+              }}
+            >
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={Map} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,65 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
+import { View } from "react-native";
+
 import { Searchbar } from "react-native-paper";
+import { FlatList } from "react-native";
+import styled from "styled-components/native";
 
 import { SafeArea } from "../../../components/utility/safe-area";
-import {
-  SearchContainer,
-  RestaurantList,
-} from "./restaurants-screen.styles";
-
-import { Spacer } from "../../../components/spacer/spacer";
 import { RestaurantInfoCard } from "../components/restaurant-info-card";
+import { Spacer } from "../../../components/spacer/spacer";
 
-////#region we moved to styled component
+import { RestaurantsContext } from "../../../services/restaurant/restaurants.context";
 
-// const SafeArea = styled(SafeAreaView)`
-//   flex: 1;
-//   ${
-//     "" /* just if StatusBar.currentHeight have a value will use this we checked by the && */
-//   }
-//   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-// `;
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 
-// const SearchContainer = styled.View`
-//   padding: ${(props) => props.theme.space[3]};
-// `;
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
 
-// const RestaurantList = styled(FlatList).attrs({
-//   contentContainerStyle: {
-//     padding: 16,
-//   },
-// })``;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
-////#endregion
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchContainer>
-      <Searchbar elevation={3} />
-    </SearchContainer>
-    <RestaurantList
-      data={[
-        { name: 1 },
-        { name: 2 },
-        { name: 3 },
-        { name: 4 },
-        { name: 5 },
-        { name: 6 },
-        { name: 7 },
-        { name: 8 },
-        { name: 9 },
-        { name: 10 },
-        { name: 11 },
-        { name: 12 },
-        { name: 13 },
-        { name: 14 },
-      ]}
-      renderItem={() => (
-        <Spacer position={"bottom"} size={"large"}>
-          <RestaurantInfoCard />
-        </Spacer>
+export const RestaurantsScreen = () => {
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+  return (
+    <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.blue600} />
+        </LoadingContainer>
       )}
-      keyExtractor={(item) => item.name}
-    />
-  </SafeArea>
-);
+
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          console.log("item " + item);
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaurant={item} />
+            </Spacer>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
+};
